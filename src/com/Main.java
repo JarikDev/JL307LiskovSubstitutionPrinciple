7684e255190f005da850b043b1430f9ae8153fed
ivan4000 <sxln1@mail.ru>
2018-12-04 15:16:26 +0300

    JL307 Liskov substitution principle

---
+++ src/com/Main.java
@@ -0,0 +1,80 @@
+package com;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+public class Main {
+    static Rectangle getRect(){
+        return new Rectangle();
+    }
+    public static void main(String[] args) {
+        Rectangle rectangle=getRect();
+        rectangle.setHeight(10);
+        rectangle.setWidth(5);
+        System.out.println(rectangle.getSquare());
+    }
+}
+interface Shape{
+    int     getSquare();
+}
+class   Rectangle implements Shape{
+    int height;
+    int width;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getSquare(){
+        return height*width;
+    }
+}
+class Square implements Shape{
+    int width;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    @Override
+    public int getSquare() {
+        return width*width;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
